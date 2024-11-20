bd1c0284587e33f8dfd3d2b007f779c45ecd4e0b
OneNull <shavrin_ns@mail.ru>
2024-11-20 17:23:39 +0300

    Neskolko visual changes in GrphicsConveyor

--- src/main/java/com/cgvsu/render_engine/GraphicConveyor.java
+++ src/main/java/com/cgvsu/render_engine/GraphicConveyor.java
@@ -4,11 +4,10 @@ import javax.vecmath.*;
 public class GraphicConveyor {
 
     public static Matrix4f rotateScaleTranslate() {
-        float[] matrix = new float[]{
-                1, 0, 0, 0,
-                0, 1, 0, 0,
-                0, 0, 1, 0,
-                0, 0, 0, 1};
+        float[] matrix = new float[]{ 1, 0, 0, 0,
+                                      0, 1, 0, 0,
+                                      0, 0, 1, 0,
+                                      0, 0, 0, 1 };
         return new Matrix4f(matrix);
     }
 
@@ -29,11 +28,10 @@ public class GraphicConveyor {
         resultY.normalize();
         resultZ.normalize();
 
-        float[] matrix = new float[]{
-                resultX.x, resultY.x, resultZ.x, 0,
-                resultX.y, resultY.y, resultZ.y, 0,
-                resultX.z, resultY.z, resultZ.z, 0,
-                -resultX.dot(eye), -resultY.dot(eye), -resultZ.dot(eye), 1};
+        float[] matrix = new float[]{ resultX.x, resultY.x, resultZ.x, 0,
+                                      resultX.y, resultY.y, resultZ.y, 0,
+                                      resultX.z, resultY.z, resultZ.z, 0,
+                                      -resultX.dot(eye), -resultY.dot(eye), -resultZ.dot(eye), 1};
         return new Matrix4f(matrix);
     }
 
@@ -49,14 +47,20 @@ public class GraphicConveyor {
         result.m22 = (farPlane + nearPlane) / (farPlane - nearPlane);
         result.m23 = 1.0F;
         result.m32 = 2 * (nearPlane * farPlane) / (nearPlane - farPlane);
+        
         return result;
     }
 
     public static Vector3f multiplyMatrix4ByVector3(final Matrix4f matrix, final Vector3f vertex) {
-        final float x = (vertex.x * matrix.m00) + (vertex.y * matrix.m10) + (vertex.z * matrix.m20) + matrix.m30;
-        final float y = (vertex.x * matrix.m01) + (vertex.y * matrix.m11) + (vertex.z * matrix.m21) + matrix.m31;
-        final float z = (vertex.x * matrix.m02) + (vertex.y * matrix.m12) + (vertex.z * matrix.m22) + matrix.m32;
-        final float w = (vertex.x * matrix.m03) + (vertex.y * matrix.m13) + (vertex.z * matrix.m23) + matrix.m33;
+        final float x = (vertex.x * matrix.m00) + (vertex.y * matrix.m10) +
+                        (vertex.z * matrix.m20) + matrix.m30;
+        final float y = (vertex.x * matrix.m01) + (vertex.y * matrix.m11) +
+                        (vertex.z * matrix.m21) + matrix.m31;
+        final float z = (vertex.x * matrix.m02) + (vertex.y * matrix.m12) +
+                        (vertex.z * matrix.m22) + matrix.m32;
+        final float w = (vertex.x * matrix.m03) + (vertex.y * matrix.m13) +
+                        (vertex.z * matrix.m23) + matrix.m33;
+        
         return new Vector3f(x / w, y / w, z / w);
     }
 
