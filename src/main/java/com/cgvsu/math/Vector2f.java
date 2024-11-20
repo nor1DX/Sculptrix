8319bec30bd6624a49bd4edd49f222a84816b3c5
artemEvdk <artemius0721@gmail.com>
2024-11-20 17:22:37 +0300

    test

--- src/main/java/com/cgvsu/math/Vector2f.java
+++ src/main/java/com/cgvsu/math/Vector2f.java
@@ -1,6 +1,6 @@
 package com.cgvsu.math;
 
-// Это заготовка для собственной библиотеки для работы с линейной алгеброй
+// Это заготовка для собственной библиотеки для работы с линейной алгеброй  и
 public class Vector2f {
     public Vector2f(float x, float y) {
         this.x = x;
